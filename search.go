09d370f4ce7363863d6d8e4652d738d1e4634dbb
Matt Winterbottom <bot.winter@gmail.com>
2018-09-26 18:57:05 +1000

    Add alpha beta pruning to search algorithm

--- search.go
+++ search.go
@@ -5,12 +5,13 @@ import (
 	"os"
 )
 
-func search(b *board, depth int, origDepth int, colour int, bestMove *move, moveStack *[]move) int {
+/* This function implements a Negamax search (see https://en.wikipedia.org/wiki/Negamax)
+with alpha beta pruning */
+func search(b *board, depth int, origDepth int, colour int, alpha int, beta int, bestMove *move, moveStack *[]move, movesProcessed int) int {
 	if depth == 0 {
 		return evaluateBoard(b, colour)
 	}
 
-	max := -999
 	score := 0
 	moves := generateAllLegalMoves(b, colour)
 	if len(moves) == 0 {
@@ -28,11 +29,15 @@ func search(b *board, depth int, origDepth int, colour int, bestMove *move, move
 	for _, move := range moves {
 		// Make the possible move
 		makeMove(b, &move)
+		movesProcessed++
 
 		// Run search again to minimise the score of the opposite colour
-		score = -search(b, depth-1, origDepth, -colour, bestMove, moveStack)
-		if score > max {
-			max = score
+		score = -search(b, depth-1, origDepth, -colour, -beta, -alpha, bestMove, moveStack, movesProcessed)
+
+		unmakeMove(b, &move)
+
+		if score > alpha {
+			alpha = score
 
 			// If we are in the root search call and this is the new best score, update bestMove
 			if depth == origDepth {
@@ -41,7 +46,7 @@ func search(b *board, depth int, origDepth int, colour int, bestMove *move, move
 
 				if cursesEnabled {
 					err := renderBoard(b)
-					err = renderStatusLine(fmt.Sprintf("Updated best move! Old best move: %s    New best move: %s with score %d", oldBestMove.toString(), bestMove.toString(), max))
+					err = renderStatusLine(fmt.Sprintf("Updated best move! Old best move: %s    New best move: %s with score %d", oldBestMove.toString(), bestMove.toString(), alpha))
 					if err != nil {
 						fmt.Println(err)
 						os.Exit(1)
@@ -49,30 +54,21 @@ func search(b *board, depth int, origDepth int, colour int, bestMove *move, move
 					handleKeyEvent()
 				}
 			}
-		} else {
-			if depth == origDepth {
-				if cursesEnabled {
-					err := renderBoard(b)
-					err = renderStatusLine(fmt.Sprintf("Move %s has score: %d    not better than current best move %s with score %d", move.toString(), score, bestMove.toString(), max))
-					if err != nil {
-						fmt.Println(err)
-						os.Exit(1)
-					}
-					handleKeyEvent()
-				}
+
+			if score >= beta {
+				break
 			}
 		}
-		unmakeMove(b, &move)
 	}
 
-	return max
+	return alpha
 }
 
 func findBestMove(b *board, colour int) move {
 	depth := 5
 	bestMove := move{0, 0, 0, 0, 0}
 	moveStack := make([]move, depth)
-	search(b, depth, depth, colour, &bestMove, &moveStack)
+	search(b, depth, depth, colour, -999, 999, &bestMove, &moveStack, 0)
 
 	if cursesEnabled {
 		err := renderBoard(b)
